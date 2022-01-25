e3f9c81e124f6c7943002d3dd8333d9d7275af84
Sandor <sincze@gmail.com>
2022-01-25 20:37:08 +0100

    Removed bug in logger function/

--- domoticzmonitor.sh
+++ domoticzmonitor.sh
@@ -38,14 +38,14 @@ else
       if [ $STATUS3 ] ; then
          exit
       else
-         NOW=$(date +"%Y-%m-%d_%H%M%S")
          sudo service domoticz.sh stop
          sleep 30
          sudo killall domoticz
-         lsof -i tcp:${DOMOTICZ_PORT} | awk 'NR!=1 {print $2}' | xargs kill -9   # Added 19-09-20187 clear used ports 8080
+         lsof -i tcp:${DOMOTICZ_PORT} | awk 'NR!=1 {print $2}' | xargs kill -9   # Added 19-09-2018 clear used ports 8080
          sudo service domoticz.sh start
          curl -s -X POST $TELEGRAM_URL -d chat_id=$TELEGRAM_ID -d text="$(echo -e "$HOSTNAME: Domoticz Restarted!")" > /dev/null 2>&1
-         logger -t "$dt, Domoticz was offline. Restarted Domoticz...."
+         logger -t Domoticz check -- Domoticz was offline. Restarted Domoticz....
+
       fi
    fi
 fi
