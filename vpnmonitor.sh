d3987f1f7a5d4d74171cc87009d18ac3bc0023ff
Sandor <sincze@gmail.com>
2024-04-28 12:54:59 +0200

    Update vpnmonitor.sh
    Updated for connect and disconnect messages and corrected error handling so logs of today will only be analyzed

--- vpnmonitor.sh
+++ vpnmonitor.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Script to monitor OpenVPN logs to see if someone logs in.
-# It will send a telegram message!
-#
-# We assume the dockername is -openvpn- if not change line 36.
+# We assume the dockername is -openvpn-
 #
 # Author: Sándor Incze
 # Version 1.0 (27-04-2024)
@@ -16,6 +14,7 @@
 # Monitor Domoticz every minute.
 # Add this line: * * * * *   root   /home/pi/scripts/vpnmonitor.sh >/dev/null 2>&1
 #
+
 
 # Source the config file
 source /home/pi/scripts/config
@@ -32,22 +31,41 @@ send_notification() {
 }
 
 parse_logs() {
-    local entry_date=$(date -d '1 min ago' "+%H:%M")
-    local logs=$(sudo /usr/bin/docker logs openvpn 2>&1 | grep "$entry_date" | grep "autologin")
+    local entry_date=$(date -d '1 minute ago' "+%Y-%m-%dT%H:%M")
+    local logs=$(sudo /usr/bin/docker logs openvpn 2>&1 | grep "$entry_date" | grep -E "autologin|client-instance exiting")
     if [[ -n "$logs" ]]; then
         echo "$logs" | while IFS= read -r line; do
-            local date=$(echo "$line" | awk '{print $1}')
-            local user=$(awk -F"'" '{print $4}' <<< "$line")
-            local cli=$(awk -F"'" '{print $6}' <<< "$line")
-            local reason=$(awk -F"'" '{print $8}' <<< "$line")
+
+            # Extract the timestamp from the log line
+            timestamp=$(echo "$line" | awk -F' ' '{print $1}')
+
+            # Convert the timestamp to a human-readable format
+            formatted_timestamp=$(date -d "$timestamp" "+%d-%m-%Y %H:%M:%S")
+
+            if [[ $line == *"client-instance exiting"* ]]; then
+                # Extract relevant information for client disconnection
+                local user=$(echo "$line" | awk -F' ' '{print $8}' | awk -F'/' '{print $1}')
+                if [[ $user == *_AUTOLOGIN ]]; then
+                    user="${user%%_AUTOLOGIN}"  # Extract username before _AUTOLOGIN
+                fi
+                local reason=$(awk -F"SIGTERM" '{print $2}' <<< "$line" | awk -F", " '{print $1}')
+                local message="vpnmonitor.sh: On $formatted_timestamp OpenVPN connection for user $user was terminated. Reason: $reason"
+            else
+                # Extract relevant information for client connection
+                local user=$(awk -F"user': '" '{print $2}' <<< "$line" | awk -F"'" '{print $1}')
+                local cli=$(awk -F"cli='" '{print $2}' <<< "$line" | awk -F"'" '{print $1}')
+                local reason=$(awk -F"reason': '" '{print $2}' <<< "$line" | awk -F"'" '{print $1}')
+                local message="vpnmonitor.sh: On $formatted_timestamp OpenVPN was accessed by user $user using a $cli client. Reason: $reason"
+            fi
+
             # Send notification
-            local message="vpnmonitor.sh: OpenVPN accessed on $date by user $user using client $cli. Reason: $reason"
             echo "$message"
             logger "$message"
             send_notification "$message"
         done
     fi
 }
+
 
 # Call parse_logs function
 parse_logs
